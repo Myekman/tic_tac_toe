a9b3f83b6458702df6a81b6db039791b1dcf7974
Myekman <my@traningstemplet.se>
2023-03-17 12:37:54 +0000

    add input and start button when to start game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
 
 
 function letsPlay() {
+
 let message1;
 document.getElementById("message1").innerHTML = "Nice, lets play! Enter your name to start"
+document.getElementById("input-name").style.display = "inline";
+document.getElementById("start-button").style.display = "inline";
+
 }
 
 function dontPlay() {
