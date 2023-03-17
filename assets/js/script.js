8b22e4d3b9e32e832b2244bffb6e574f36c7eca7
Myekman <my@traningstemplet.se>
2023-03-17 11:35:52 +0000

    message to user when click on buttons

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+
+function letsPlay() {
+let message1;
+document.getElementById("message1").innerHTML = "Nice, lets play! Enter your name to start"
+}
+
+function dontPlay() {
+    let message2;
+    document.getElementById("message2").innerHTML = "That's too bad.. Maybee another time!"  
+}
