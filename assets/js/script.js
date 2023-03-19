1e1a45034951d45081ade03fb6d07852485d88d7
Myekman <my@traningstemplet.se>
2023-03-19 16:46:25 +0000

    add if else code to start game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,41 @@
 
+/* choise the button to play */ 
 
 function letsPlay() {
 
     let message2;
-    document.getElementById("message2").innerHTML = "Nice, lets play! Enter your name to start"
+    document.getElementById("message").innerHTML = "Nice, lets play! Enter your name to start"
     let name=document.querySelector("#name-section");
     name.style.display = "block";
     document.getElementById("input-name").style.display = "inline";
     document.getElementById("start-button").style.display = "inline";
 }
 
+/* choise the button not to play */ 
+
 function dontPlay() {
     let message2;
-    document.getElementById("message2").innerHTML = "That's too bad.. Maybee another time!"  
-    let name=document.querySelector("#name-section");
-    name.style.display = "none";
-}
-
-function start() {
-    let message=document.querySelector("#message-section");
-    message.style.display = "none";
+    document.getElementById("message").innerHTML = "That's too bad.. Maybee another time!"  
     let name=document.querySelector("#name-section");
     name.style.display = "none";
     let welcome=document.querySelector("#welcome-section");
-    welcome.style.display = "none";
+    welcome.style.display= "none";
+}
+
+/* Enter your name and start game */
+
+function start() {
+    let div = document.getElementById("fist-page");
+    let display = 0; 
+
+    if(display ==1)
+    {
+        div.style.display = 'block';
+        display = 0;
+    }
+    else 
+    {
+        div.style.display = 'none';
+        display = 1;
+    }
 }
