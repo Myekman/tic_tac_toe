0487900b957668c75d77460ec4258fd1e49734c3
Myekman <my@traningstemplet.se>
2023-03-19 12:01:56 +0000

    add code to show/hide section

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,26 @@
 
 function letsPlay() {
 
-let message1;
-document.getElementById("message1").innerHTML = "Nice, lets play! Enter your name to start"
-document.getElementById("input-name").style.display = "inline";
-document.getElementById("start-button").style.display = "inline";
-
+    let message2;
+    document.getElementById("message2").innerHTML = "Nice, lets play! Enter your name to start"
+    let name=document.querySelector("#name-section");
+    name.style.display = "block";
+    document.getElementById("input-name").style.display = "inline";
+    document.getElementById("start-button").style.display = "inline";
 }
 
 function dontPlay() {
     let message2;
     document.getElementById("message2").innerHTML = "That's too bad.. Maybee another time!"  
+    let name=document.querySelector("#name-section");
+    name.style.display = "none";
+}
+
+function start() {
+    let message=document.querySelector("#message-section");
+    message.style.display = "none";
+    let name=document.querySelector("#name-section");
+    name.style.display = "none";
+    let welcome=document.querySelector("#welcome-section");
+    welcome.style.display = "none";
 }
