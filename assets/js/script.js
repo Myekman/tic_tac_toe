68d546d6cb42671e756c50f3d696bf43c114e5af
Myekman <my@traningstemplet.se>
2023-03-21 11:31:00 +0000

    add message of x's turn or o's turn when playing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,36 @@
+// Get Buttons
+let yesBtn = document.querySelector('#yes')
+let noBtn = document.querySelector('#no')
+let restartBtn = document.getElementById('restartbtn')
+
+let winningMessage = document.getElementById('winningMessage')
+
+// Get array from boxes in gameboard
+let boxes = Array.from(document.getElementsByClassName('box'))
+let spaces = Array(9).fill(null)
+
+const O_TEXT = "O"
+const X_TEXT = "X"
+let currentPlayer = X_TEXT
+let winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
 /* clicked button to play */
 function letsPlay() {
 
-    document.getElementById("message").innerHTML = "Nice, lets play! Enter your name to start"
+    document.getElementById("message").innerHTML = "Thats fun! Enter your name to start"
     let name = document.querySelector("#name-section");
     name.style.display = "block";
     document.getElementById("input-name").style.display = "inline";
+    document.getElementById("input-name2").style.display = "inline";
     document.getElementById("start-button").style.display = "inline";
 
     let welcome = document.querySelector("#welcome-section");
@@ -20,7 +46,6 @@ function dontPlay() {
     let welcome = document.querySelector("#welcome-section");
     welcome.style.display = "none";
 }
-
 /* Enter your name and start game */
 function start() {
     if (document.getElementById("input-name").value.length == 0) {
@@ -42,17 +67,17 @@ function start() {
         game.style.display = "block";
         let players = document.querySelector("#player-section");
         players.style.display = "block";
+
+        /* will display name on player when enter game */
+        let player1= document.getElementById("input-name").value;
+        document.getElementById("displayName").innerHTML = `<h2>${player1}</h2>`;
+
+        let player2 = document.getElementById("input-name2").value;
+        document.getElementById("displayName2").innerHTML = `<h2>${player2}</h2>`;
     }
 }
 
-
-let restartBtn = document.getElementById('restartbtn')
-let boxes = Array.from(document.getElementsByClassName('box'))
-
-const O_TEXT = "O"
-const X_TEXT = "X"
-let currentPlayer = X_TEXT
-let spaces = Array(9).fill(null)
+/* Start game when clicking on board */
 
 const startGame = () => {
     boxes.forEach(box => box.addEventListener('click', boxClicked))
@@ -67,21 +92,39 @@ function boxClicked(square) {
         spaces[id] = currentPlayer
         square.target.innerText = currentPlayer
 
-        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT
+        if (playerHasWon()) {
+            winningMessage.textContent = `${currentPlayer} has won!`;
+        }
+
+        currentPlayer = (currentPlayer == "X") ? "O" : "X";
+        winningMessage.textContent = `${currentPlayer}'s turn`;
     }
 
 }
+
+/* restart buttun will clear the board */
 
 restartBtn.addEventListener('click', restart)
 
 function restart() {
     spaces.fill(null)
 
-    boxes.forEach( box => {
+    boxes.forEach(box => {
         box.innerText = ""
     })
+
+    winningMessage = 'Tic Tac Toe'
     currentPlayer = X_TEXT
 
 }
+
+function playerHasWon() {
+
+
+}
+
+
+
+
 
 startGame()
